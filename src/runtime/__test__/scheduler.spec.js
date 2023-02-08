57d217b848040aedd7eb3c098c8450052e58ca7b
D城兔Maziyo <247948681@qq.com>
2023-02-08 14:25:36 +0800

    chore:testCase finished

--- src/runtime/__test__/scheduler.spec.js
+++ src/runtime/__test__/scheduler.spec.js
@@ -64,4 +64,105 @@ describe('scheduler', () => {
     expect(div.innerHTML).toBe('9')
     expect(spy).toHaveReturnedTimes(3)
   })
+  test('multi async mutation will render only once', async () => {
+    jest.useFakeTimers()
+    const spy = jest.fn()
+    const Comp = createComp(spy)
+    render(h(Comp), root)
+    expect(spy).toHaveReturnedTimes(1)
+
+    const div = root.children[0]
+    const asyncBtn = root.children[2]
+
+    asyncBtn.click()
+
+    // 人工模拟暂停
+    await jest.advanceTimersByTime(150)
+    expect(div.innerHTML).toBe('3')
+    expect(spy).toHaveBeenCalledTimes(2)
+
+    asyncBtn.click()
+    await jest.advanceTimersByTime(150)
+    expect(div.innerHTML).toBe('6')
+    expect(spy).toHaveBeenCalledTimes(3)
+  })
+
+  test('two sibling components can render correctly', async () => {
+    const spy1 = jest.fn()
+    const spy2 = jest.fn()
+
+    const ref1 = ref(0)
+    const ref2 = ref(10)
+    const Comp1 = {
+      render() {
+        spy1()
+        return h('div', null, ref1.value)
+      }
+    }
+    const Comp2 = {
+      render() {
+        spy2()
+        return h('div', null, ref2.value)
+      }
+    }
+    render(h(Fragment, null, [h(Comp1), h(Comp2)]), root)
+
+    expect(root.innerHTML).toBe('<div>0</div><div>10</div>')
+    expect(spy1).toHaveBeenCalledTimes(1)
+    expect(spy2).toHaveBeenCalledTimes(1)
+
+    ref1.value++
+    ref1.value++
+    ref2.value++
+    ref2.value++
+
+    await nextTick()
+    expect(root.innerHTML).toBe('<div>2</div><div>12</div>')
+    expect(spy1).toHaveBeenCalledTimes(2)
+    expect(spy2).toHaveBeenCalledTimes(2)
+  })
+  test('two nested component can render correctly', async () => {
+    const spy1 = jest.fn()
+    const spy2 = jest.fn()
+    const ref1 = ref(0)
+    const ref2 = ref(10)
+
+    const Child = {
+      props: ['value'],
+      render(ctx) {
+        spy2()
+        return h('div', null, ctx.value + ref2.value)
+      }
+    }
+    const Parent = {
+      render() {
+        spy1()
+        return h(Child, { value: ref1.value })
+      }
+    }
+
+    render(h(Parent), root)
+    expect(root.innerHTML).toBe('<div>10</div>')
+    expect(spy1).toHaveBeenCalledTimes(1)
+    expect(spy2).toHaveBeenCalledTimes(1)
+
+    ref1.value++
+    ref1.value++
+
+    await nextTick()
+    expect(spy1).toHaveBeenCalledTimes(2)
+    expect(spy2).toHaveBeenCalledTimes(2)
+
+    ref1.value++
+    ref1.value++
+    ref2.value += 10
+    ref2.value += 10
+    await nextTick()
+    expect(root.innerHTML).toBe('<div>34</div>')
+
+    expect(spy1).toHaveBeenCalledTimes(3)
+
+    // 第三次是被动更新的,第四次是主动更新一次
+    expect(spy2).toHaveBeenCalledTimes(4)
+  })
 })
